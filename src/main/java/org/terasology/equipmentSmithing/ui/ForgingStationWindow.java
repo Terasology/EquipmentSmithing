2052444b735b2ca81d7b3e647fe08b6b78463c40
DarkWeird <foxflameless@gmail.com>
2020-08-24 16:10:59 +0300

    fix: NUI extraction compability

--- src/main/java/org/terasology/equipmentSmithing/ui/ForgingStationWindow.java
+++ src/main/java/org/terasology/equipmentSmithing/ui/ForgingStationWindow.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.equipmentSmithing.ui;
 
 import org.terasology.engine.Time;
@@ -21,18 +8,17 @@ import org.terasology.equipmentSmithing.EquipmentSmithing;
 import org.terasology.heat.component.HeatProducerComponent;
 import org.terasology.heat.ui.ThermometerWidget;
 import org.terasology.logic.players.LocalPlayer;
+import org.terasology.nui.UIWidget;
+import org.terasology.nui.databinding.Binding;
+import org.terasology.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.widgets.ActivateEventListener;
+import org.terasology.nui.widgets.UIButton;
+import org.terasology.nui.widgets.UILoadBar;
 import org.terasology.processing.ui.VerticalTextureProgressWidget;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.nui.BaseInteractionScreen;
 import org.terasology.rendering.nui.NUIManager;
-import org.terasology.rendering.nui.UIWidget;
-import org.terasology.rendering.nui.databinding.Binding;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.ingame.inventory.InventoryGrid;
-import org.terasology.rendering.nui.widgets.ActivateEventListener;
-import org.terasology.rendering.nui.widgets.UIButton;
-import org.terasology.rendering.nui.widgets.UILoadBar;
-import org.terasology.smithing.Smithing;
 import org.terasology.workstation.component.WorkstationProcessingComponent;
 import org.terasology.workstation.event.WorkstationProcessRequest;
 import org.terasology.workstation.process.WorkstationProcess;
@@ -274,8 +260,7 @@ public class ForgingStationWindow extends BaseInteractionScreen {
 
                 // Before checking if the workstation has the necessary items in the upgrade slot, check to see if this
                 // upgrade process actually pertains to this workstation type.
-                if (upgradeProcess.getWorkstationType().equalsIgnoreCase(workstation.getParentPrefab().getName()))
-                {
+                if (upgradeProcess.getWorkstationType().equalsIgnoreCase(workstation.getParentPrefab().getName())) {
                     UpgradeRecipe upgradeRecipe = upgradeProcess.getUpgradeRecipe();
                     final UpgradeRecipe.UpgradeResult upgradeResult = upgradeRecipe.getMatchingUpgradeResult(workstation);
 
